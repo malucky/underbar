6b7b6c54b7bcff800923f59c437c21bfa1f230de
Ray Ma <rhm262@gmail.com>
2013-10-09 16:11:37 -0400

    Implemented _.extend and _.default successfully.

--- src/underbar.js
+++ src/underbar.js
@@ -251,11 +251,33 @@ var _ = { };
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    /* //simple implementation
+    for (var i = 1; i < arguments.length; i++){
+      for (var key in arguments[i]) {
+        obj[key] = arguments[i][key];
+      }
+    }
+    return obj;
+    //simple implementation end */
+    _.each(arguments, function(object){
+      for (var key in object){
+        obj[key] = object[key];
+      }
+    })
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    _.each(arguments, function(object){
+      for (var key in object){
+        if (obj[key] === undefined){
+          obj[key] = object[key];
+        }
+      }
+    })
+    return obj;
   };
 
 
