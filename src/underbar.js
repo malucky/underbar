dd4420e805e5fc63b148b1e20085f501e8cc7d50
Ray Ma <rhm262@gmail.com>
2013-10-09 15:52:34 -0400

    Implemented _.some successfully.

--- src/underbar.js
+++ src/underbar.js
@@ -216,6 +216,19 @@ var _ = { };
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (collection.length === 0){
+      return false;
+    }
+    if (iterator === undefined){
+      iterator = function(item){
+        return Boolean(item);
+      }
+    }
+    //some if opposite of every is false
+    return _.every(collection, function(item){
+      return !iterator(item);
+    }) === false;
+
   };
 
 
